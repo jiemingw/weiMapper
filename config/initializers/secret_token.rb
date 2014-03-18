389f170c2c85017422e91920b9f4113c5ca40ed8
Dominik Rastawicki <rastawicky@yahoo.com>
2014-03-17 19:54:37 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeiMapper::Application.config.secret_key_base = '8fe321a0f72e707576479586c8e6c59b0aff5a23afdba7331bae23437348d63c17c467252fdb2360f9fd047dc628dced775f75cc6b2553ae91f1108b77598eff'
